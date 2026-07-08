50ac834a9ae1ea7647b6424b5a288388a6048771
adtshrm007 <adtshrm1@gmail.com>
2026-07-08 12:26:09 +0530

    Added new routes and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { ToastProvider } from "./context/ToastContext.jsx";
 import ProtectedRoute from "./Components/common/ProtectedRoute.jsx";
 
 import Home from "./pages/Home";
@@ -11,74 +12,85 @@ import AnalysisHistory from "./pages/AnalysisHistory.jsx";
 import V15Dashboard from "./pages/V15Dashboard.jsx";
 import SettingsPage from "./pages/SettingsPage.jsx";
 import CodeExplorer from "./pages/CodeExplorer.jsx";
+import CompareScans from "./pages/CompareScans.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          {/* Public routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
+        <ToastProvider>
+          <Routes>
+            {/* Public routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
 
-          {/* Protected dashboard routes */}
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/repositories"
-            element={
-              <ProtectedRoute>
-                <Repositories />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/repositories/:owner/:repo"
-            element={
-              <ProtectedRoute>
-                <RepoExplorer />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/analysis"
-            element={
-              <ProtectedRoute>
-                <AnalysisHistory />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/scan/:id"
-            element={
-              <ProtectedRoute>
-                <V15Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/settings"
-            element={
-              <ProtectedRoute>
-                <SettingsPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/explorer"
-            element={
-              <ProtectedRoute>
-                <CodeExplorer />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+            {/* Protected dashboard routes */}
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/repositories"
+              element={
+                <ProtectedRoute>
+                  <Repositories />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/repositories/:owner/:repo"
+              element={
+                <ProtectedRoute>
+                  <RepoExplorer />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/analysis"
+              element={
+                <ProtectedRoute>
+                  <AnalysisHistory />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/compare"
+              element={
+                <ProtectedRoute>
+                  <CompareScans />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/scan/:id"
+              element={
+                <ProtectedRoute>
+                  <V15Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/settings"
+              element={
+                <ProtectedRoute>
+                  <SettingsPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/explorer"
+              element={
+                <ProtectedRoute>
+                  <CodeExplorer />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </ToastProvider>
       </AuthProvider>
     </BrowserRouter>
   );
